b41c0a6cd9ff53277d6a404bd6d85713f7b05f14
Dylan <dylansamuelwright@gmail.com>
2014-02-20 06:58:39 -0800

    updated homepage route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Stocktators::Application.routes.draw do
-  get "static_pages/home"
+  resources :users
+  root 'static_pages#home'
   get "static_pages/newstock"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
