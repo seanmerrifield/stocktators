56c1a4903d6cbba399131b1711ffb03cd63953e4
seanmerrifield <seanmerrifield@gmail.com>
2014-03-09 18:29:36 -0700

    Added Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	let(:base_title) { "Stocktators" }
+
 	describe "Home page" do
 
 		it "should have the content 'Stocktators' " do
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Stocktators | Home")
+			expect(page).to have_title("#{base_title} | Home")
 		end
 	end
 
@@ -24,7 +26,7 @@ describe "StaticPages" do
 
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			expect(page).to have_title("Stocktators | Help")
+			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
@@ -37,9 +39,23 @@ describe "StaticPages" do
 
 		it "should have the title 'About Us' " do
 			visit '/static_pages/about'
-			expect(page).to have_title("Stocktators | About Us")
+			expect(page).to have_title("#{base_title} | About Us")
 		end
 	end
+
+	describe "Contact page" do
+
+		it "should have the content 'Contact Us' " do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact Us')
+		end
+
+		it "should have the title 'Contact Us' " do
+			visit '/static_pages/contact'
+			expect(page).to have_title("#{base_title} | Contact Us")
+		end
+	end
+
 
   	describe "Stock insert page" do
 
