6d741c9a663fb75f64e3181197216933e1e28202
seanmerrifield <seanmerrifield@gmail.com>
2014-03-22 21:59:16 -0700

    Finish user signup routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Stocktators::Application.routes.draw do
+  get "users/new"
+
   root 'static_pages#home'
+  match '/signup',  to: 'users#new',              via: 'get'
   match '/help',    to: 'static_pages#help',      via: 'get'
   match '/about',   to: 'static_pages#about',     via: 'get'
   match '/contact', to: 'static_pages#contact',   via: 'get'
   match 'newstock', to: 'static_pages#newstock',  via: 'get'
 
-  get "users/new"
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
